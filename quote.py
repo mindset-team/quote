76322716fe7b2a0d52f5225d207eed871b6cc578
mindset.io team <team@mindset.io>
2021-12-04 10:43:25 -0800

    Update quote.py

--- quote.py
+++ quote.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from datetime import datetime as dt
-from pprint import pprint
+from pathlib import Path
 from string import Template
 
 import requests
@@ -10,28 +10,18 @@ QUOTE_URL = "https://api.quotable.io/random"
 
 
 def get_quote():
-    print(f"Fetching quote from {QUOTE_URL}")
     resp = requests.get(QUOTE_URL).json()
     qdct = {
         "quote": resp["content"],
         "author": resp["author"],
+        "text": f'{resp["content"]}\n - {resp["author"]}',
         "utc_time": f"{dt.utcnow().isoformat()[:-7]}Z",
     }
-    with open("template.html", "r") as f:
-        tmpl = Template(f.read())
-    text = f'{qdct["quote"]}\n - {qdct["author"]}'
-    html = tmpl.substitute(qdct)
-    qdct |= {
-        "html": html,
-        "text": text,
-        "email": {
-            "subject": "you've got a quote!",
-            "plain": text,
-            "html": html,
-        },
-    }
+    tmpl = Template(Path("template.html").read_text())
+    qdct['html'] = tmpl.substitute(qdct)
     return qdct
 
 
 if __name__ == "__main__":  # Local testing
+    from pprint import pprint
     pprint(get_quote())
